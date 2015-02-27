f8cad9bde2be6104cc938996c47d0a755e51c8d0
Viral Mehta <vmehta211@gmail.com>
2015-02-26 23:29:35 -0800

    added db config
    this is where i'll store auth tokens

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+ $db_config =  array(
+        'host'              => '127.0.0.1',
+        'username'          => 'usernamehere',
+        'password'          => 'passwordhere',
+        'dbname'            => 'dbnamehere'
+        );
+
+ 
